c1d862bcdec1d971dbdb7083fea4c83b359f8945
SheriffHobo <zombiemaster@gmail.com>
2019-02-24 17:49:43 -0800

    many updates, now killing bugs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,11 +8,13 @@ $(function () {
         $('#breed-btns').empty();
         dogBreeds.forEach(breed => {
             var buttons = $('<button>').text(breed);
+            buttons.attr("id", "dog-btn");
+            buttons.attr("data-name", breed.replace(/ /g, "_"));
             $('#breed-btns').prepend(buttons);
         });
     };
 
-    // CLICK LISTENER
+    // ADD NEW BREED LISTENER
     $("#add-breed").on("click", function (event) {
         event.preventDefault();
         var userInput = $('#newbreedfield').val().trim();
@@ -24,30 +26,39 @@ $(function () {
         };
         renderButtons();
     });
+
     renderButtons();
 
-    // CLEAR THE PAGE
-    $("#clear-page").click(function() {
+    // CLEAR PAGE BUTTON
+    $("#clear-page").click(function () {
         document.location.reload(true);
     });
 
+    // AJAX QUERY
+    $("#dog-btn").on("click", function () { // Only works for Beagle, sometimes. But never works for any other pre-loaded, or added buttons
+        $('#gif-area').empty();
+        var dataName = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=OQyMsj80Q5Vaeop0ry2jPaOAFqI8Isis&rating=g&q=" + dataName;
+        console.log(queryURL);
 
-
-
-
-
-
-    // GIF start/stop
-    // $(".gif").on("click", function () {
-
-    // var state = $(this).attr("data-state");
-    // if (state === "still") {
-    //     $(this).attr("data-state", "animate");
-    //     $(this).attr("src", $(this).attr("data-animate"))
-    // } else {
-    //      $(this).attr("data-state", "still");
-    //     $(this).attr("src", $(this).attr("data-still"))
-    // };
-    // });
-
+        $.ajax({
+            url: queryURL, 
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+                    var dogImage = $("<img>");
+                    dogImage.attr("id", "dogpics"); // Adds an ID to each gif, but the CSS doesnt come through
+                    dogImage.attr("src", results[i].images.fixed_height.url);
+                    gifDiv.prepend(dogImage);
+                    $("#gif-area").prepend(gifDiv);
+                };
+            });
+    });
 });
+
+    // Need a way to clear the breed field when Add is clicked
+
+    // Do nothing if numbers are entered
